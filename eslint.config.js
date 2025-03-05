ad68ab9f10ddcdd3b72d2085e3221a94f188e604
Marc Nuri <marc@marcnuri.com>
2025-03-05 07:30:40 +0100

    fix: remove unnecessary directory ignore from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import globals from 'globals';
 import js from '@eslint/js';
 
 export default [
-  {ignores: ['dist/', 'node_modules_unix_arm64/', 'node_modules_windows_x64/']},
+  {ignores: ['dist/', 'node_modules_windows_x64/']},
   js.configs.recommended,
   {
     languageOptions: {
