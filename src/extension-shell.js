2a040e7e31ef3475a76deba764891d6de3a6eb78
Marc Nuri <marc@marcnuri.com>
2025-03-11 13:41:35 +0100

    feat: just use shell settings

--- src/extension-shell.js
+++ src/extension-shell.js
@@ -3,28 +3,25 @@ const os = require('node:os');
 
 // Run the shell command in bash so that any .bashrc or .bash_profile settings are applied
 const preferBash = ({file, args}) => {
+  let shell;
   if (os.platform() !== 'win32') {
     try {
       fs.statSync('/bin/bash');
-      return {
-        file: '/bin/bash',
-        args: ['-c', `"${file} ${args.join(' ')}"`],
-        shell: '/bin/bash'
-      };
+      shell = '/bin/bash';
     } catch {
       // No bash, just return the original command
     }
   }
-  return {file, args};
+  return {file, args, shell};
 };
 
-export const spawnShell = (originalFile, originalArgs, {tty = false} = {}) => {
+export const spawnShell = (originalFile,  originalArgs = [], {tty = false} = {}) => {
   const {file, args, shell} = preferBash({
     file: originalFile,
     args: originalArgs
   });
   const {spawn} = require('node:child_process');
-  console.log(`Spawning ${file} ${args.join(' ')}`);
+  console.log(`Spawning ${file} ${args.join(' ')} (shell: ${shell})`);
   const spawnProcess = spawn(file, args, {
     shell,
     cwd: process.env.HOME,
@@ -51,13 +48,13 @@ export const spawnShell = (originalFile, originalArgs, {tty = false} = {}) => {
   };
 };
 
-export const spawnShellSync = (originalFile, originalArgs) => {
+export const spawnShellSync = (originalFile, originalArgs = []) => {
   const {file, args, shell} = preferBash({
     file: originalFile,
     args: originalArgs
   });
   const {spawnSync} = require('node:child_process');
-  console.log(`Spawning sync ${file} ${args.join(' ')}`);
+  console.log(`Spawning sync ${file} ${args.join(' ')} (shell: ${shell})`);
   return spawnSync(file, args, {
     shell,
     cwd: process.env.HOME,
