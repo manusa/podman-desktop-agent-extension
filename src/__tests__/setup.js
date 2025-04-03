cd3c8e675f489c2979ff387b4ff600e4b78a40cb
Marc Nuri <marc@marcnuri.com>
2025-04-03 14:40:00 +0200

    test: ai-sdk initial tests

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -6,8 +6,12 @@ vi.mock('node:child_process');
 beforeEach(() => {
   // console mocks
   console.logs = '';
+  console.errors = '';
   vi.spyOn(console, 'log').mockImplementation((...args) => {
     console.logs += args.join(' ') + '\n';
+  });
+  vi.spyOn(console, 'error').mockImplementation((...args) => {
+    console.errors += args.join(' ') + '\n';
   });
   // default node:child_process mocks
   vi.mocked(spawn).mockImplementation(() => ({
---
+++ src/ai-sdk.test.js
@@ -0,0 +1,78 @@
+import {afterEach, beforeEach, describe, expect, test, vi} from 'vitest';
+import {findFreePort} from './net.js';
+import {newAiSdk} from './ai-sdk.js';
+
+describe('ai-sdk', () => {
+  let configuration;
+  beforeEach(() => {
+    configuration = {
+      load: vi.fn()
+    };
+  });
+  describe('start()', () => {
+    let aiSdk;
+    beforeEach(async () => {
+      configuration.aiSdkPort = await findFreePort();
+      aiSdk = newAiSdk({configuration});
+      aiSdk.start();
+    });
+    afterEach(() => {
+      aiSdk.close();
+    });
+    test('logs server starting', async () => {
+      expect(console.logs).toContain('Ai SDK: Starting...');
+    });
+    test('sets up express server', () => {
+      expect(aiSdk._server).not.toBeNull();
+    });
+    test('listens on configured port', async () => {
+      expect(aiSdk._server.address().port).toBe(configuration.aiSdkPort);
+      expect(aiSdk._server.listening).toBe(true);
+    });
+  });
+  describe('close()', () => {
+    let aiSdk;
+    beforeEach(async () => {
+      configuration.aiSdkPort = await findFreePort();
+      aiSdk = newAiSdk({configuration});
+      aiSdk.start();
+      await aiSdk.close();
+    });
+    test('logs server closing', async () => {
+      expect(console.logs).toContain('Ai SDK: Closing...');
+    });
+    test('closes the server', () => {
+      expect(aiSdk._server.listening).toBe(false);
+    });
+  });
+  describe('cors', () => {
+    let aiSdk;
+    let response;
+    beforeEach(async () => {
+      configuration.aiSdkPort = await findFreePort();
+      aiSdk = newAiSdk({configuration});
+      aiSdk.start();
+      response = await fetch(
+        `http://localhost:${configuration.aiSdkPort}/api/v1/messages`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({message: 'test'})
+        }
+      );
+    });
+    afterEach(() => {
+      aiSdk.close();
+    });
+    test('Has Access-Control-Allow-Origin=*', async () => {
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe('*');
+    });
+    test('Has Access-Control-Allow-Headers', async () => {
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(
+        'Origin, X-Requested-With, Content-Type, Accept'
+      );
+    });
+  });
+});
