772b13b0c1fbd5ccd0ce340f7e8c5ac61b794571
Marc Nuri <marc@marcnuri.com>
2025-04-04 17:34:58 +0200

    refactor(assistant-ui): removed unneeded ThreadWelcome

--- assistant-ui/src/ThreadWelcome.jsx
+++
@@ -1,9 +0,0 @@
-import {ThreadPrimitive} from '@assistant-ui/react';
-
-export const ThreadWelcome = ({message = 'How can I help you today?'}) => {
-  return (
-    <ThreadPrimitive.Empty>
-      <div>{message}</div>
-    </ThreadPrimitive.Empty>
-  );
-};
